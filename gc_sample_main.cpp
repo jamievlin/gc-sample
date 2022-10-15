acec4062af61c07312fab882e8996999b621f612
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2022-10-15 15:22:53 -0600

    CXX: Add sample gc code.

--- gc_sample_main.cpp
+++ gc_sample_main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <memory>
 
-int main(int argc, char* argv)
+#include "gcservice/gcservice.h"
+
+class Something
 {
-    std::cout << "hello from bazel!";
+public:
+    Something (int const& va)
+        : a(va),
+        b(va + 100)
+    {
+
+    }
+
+    void print()
+    {
+        std::cout << a << " " << b;
+    }
+private:
+    int a, b;
+};
+
+int main(int argc, char* argv[])
+{
+    for (int i = 0; i < 10000000; ++i)
+    {
+        auto* something = gcservice::allocateClassGC<Something>(155);
+        something->print();
+    }
 }
--- gcservice/gcservice.h
+++ gcservice/gcservice.h
@@ -1,2 +1,24 @@
 #pragma once
 
+#include <gc.h>
+#include <type_traits>
+
+namespace gcservice
+{
+template<typename T>
+T* allocateGc()
+{
+    return static_cast<T*>(GC_MALLOC(sizeof(T)));
+}
+
+template<
+        typename T,
+        typename TAnon = std::enable_if_t<std::is_trivially_destructible_v<T>>,
+        typename ... TCtorArgs
+        >
+T* allocateClassGC(TCtorArgs&& ... args)
+{
+    void* rawPtr = GC_MALLOC(sizeof(T));
+    return new (rawPtr) T(std::forward<TCtorArgs>(args)...);
+}
+};
